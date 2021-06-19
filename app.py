138321f651705930885bda3af413c5dd3cddbb27
Talha Abdur Rahman <cooltalha27@gmail.com>
2021-06-20 00:21:28 +0530

    Updates for 2021-06-20 00:21:28

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'omg, it actually worked'
+    return 'omg, it actually worked !!!!!!1'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
